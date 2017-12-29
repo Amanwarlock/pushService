91dba91c584d334c36ef86c15a382c2230d34067
amankareem <aman.kareem@storeking.in>
2017-12-29 22:36:05 +0530

    changes

--- api/controllers/add.device.ctrl.js
+++ api/controllers/add.device.ctrl.js
@@ -7,6 +7,7 @@ var definition = require("../helpers/device.definition");
 function addDevice (req , res){
     var _params = req.swagger.params['data'].value;
     console.log("-----inside add Device----" , _params);
+    res.status(200).send({message : "success" , data : _params});
 }
 
 module.exports = {
